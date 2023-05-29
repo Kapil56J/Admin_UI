cc8cda5eb2e0dfa4766d6ea6e9770e612385a1e7
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2023-05-29 10:23:52 +0530

    Paginations.js

--- src/Component/Paginations.js
+++ src/Component/Paginations.js
@@ -1,38 +1,54 @@
+
+
 import { Pagination } from "react-bootstrap";
 
 const Paginations = ({ currentPage, setPage, totalPages }) => {
-  const pageNumbers = [];
+  const generatePageNumbers = () => {
+    const pageNumbers = [];
 
-  for (let i = 1; i <= totalPages; i++) {
-    pageNumbers.push(i);
-  }
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
+
+    return pageNumbers;
+  };
+
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === totalPages;
+
+  const renderPaginationItem = (number) => {
+    const isActive = number === currentPage;
+
+    return (
+      <Pagination.Item
+        className={isActive ? "active" : ""}
+        onClick={() => setPage(number)}
+        key={number}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  };
 
   return (
     <Pagination className="float-md-center d-flex justify-content-center pt-2">
       <Pagination.First
-        className={currentPage === 1 ? "disabled" : ""}
+        className={isFirstPage ? "disabled" : ""}
         onClick={() => setPage(1)}
       />
       <Pagination.Prev
-        className={currentPage === 1 ? "disabled" : ""}
+        className={isFirstPage ? "disabled" : ""}
         onClick={() => setPage(currentPage - 1)}
       />
 
-      {pageNumbers.map((number) => (
-        <Pagination.Item
-          className={number === currentPage ? "active" : ""}
-          onClick={() => setPage(number)}
-          key={number}
-        >
-          {number}
-        </Pagination.Item>
-      ))}
+      {generatePageNumbers().map(renderPaginationItem)}
+
       <Pagination.Next
-        className={currentPage === totalPages ? "disabled" : ""}
+        className={isLastPage ? "disabled" : ""}
         onClick={() => setPage(currentPage + 1)}
       />
       <Pagination.Last
-        className={currentPage === totalPages ? "disabled" : ""}
+        className={isLastPage ? "disabled" : ""}
         onClick={() => setPage(totalPages)}
       />
     </Pagination>
